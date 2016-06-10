fe885cc2e192913a7b7db0d40574bce83ea3f356
Alexander G. Pronchenkov <alexander@pronchenkov.name>
2016-06-11 00:15:50 +0100

    clang-format

--- sequential_insert.cpp
+++ sequential_insert.cpp
@@ -1,15 +1,16 @@
 #include "google_btree/btree_set.h"
+#include <algorithm>
+#include <random>
 #include <set>
 #include <unordered_set>
-#include <random>
-#include <algorithm>
 
 using Duration = std::chrono::duration<double>;
 
 auto Now = [] { return std::chrono::high_resolution_clock::now(); };
 
 // Sequential insert
-template <class IntegerSet> Duration SequentialInsertTestImpl(IntegerSet set, int n) {
+template <class IntegerSet>
+Duration SequentialInsertTestImpl(IntegerSet set, int n) {
   auto start = Now();
   for (int i = 0; i < n; ++i) {
     set.insert(i);
@@ -17,28 +18,28 @@ template <class IntegerSet> Duration SequentialInsertTestImpl(IntegerSet set, in
   return Now() - start;
 }
 
-
-template <class IntegerSet> Duration SequentialInsertTest(IntegerSet set, int n, int k = 32) {
-    auto result = SequentialInsertTestImpl(set, n);
-    for (int i = 1; i < k; ++i) {
-        result = std::min(result, SequentialInsertTestImpl(set, n));
-    }
-    return result;
+template <class IntegerSet>
+Duration SequentialInsertTest(IntegerSet set, int n, int k = 32) {
+  auto result = SequentialInsertTestImpl(set, n);
+  for (int i = 1; i < k; ++i) {
+    result = std::min(result, SequentialInsertTestImpl(set, n));
+  }
+  return result;
 }
-
 
 int main() {
   for (int n = 256; n <= 1024 * 1024; n += n) {
-      std::set<int> set;
-      std::unordered_set<int> unordered_set_reserved;
-      std::unordered_set<int> unordered_set;
-      btree::btree_set<int> btree_set;
-      unordered_set_reserved.reserve(n);
-      std::cout << n << '\t'
-                << SequentialInsertTest(std::move(btree_set), n).count() << '\t'
-                << SequentialInsertTest(std::move(unordered_set_reserved), n).count() << '\t'
-                << SequentialInsertTest(std::move(unordered_set), n).count() << '\t'
-                << SequentialInsertTest(std::move(set), n).count() << std::endl;
+    std::set<int> set;
+    std::unordered_set<int> unordered_set_reserved;
+    std::unordered_set<int> unordered_set;
+    btree::btree_set<int> btree_set;
+    unordered_set_reserved.reserve(n);
+    std::cout
+        << n << '\t' << SequentialInsertTest(std::move(btree_set), n).count()
+        << '\t'
+        << SequentialInsertTest(std::move(unordered_set_reserved), n).count()
+        << '\t' << SequentialInsertTest(std::move(unordered_set), n).count()
+        << '\t' << SequentialInsertTest(std::move(set), n).count() << std::endl;
   }
   return 0;
 }
